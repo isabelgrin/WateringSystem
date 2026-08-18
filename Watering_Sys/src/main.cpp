1ee716aef5fc0474e270ffb4040c9fb8b2643bcc
I-Gringeri <igringer@stevens.edu>
2026-08-18 19:33:42 -0400

    preliminary prgram for measuring sensor data. Will do testing with xiao once set up moisture sensors for each plant

--- Watering_Sys/src/main.cpp
+++ Watering_Sys/src/main.cpp
@@ -1,18 +1,68 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+
+//moisture sensor
+//will make more counters when there are several motors and/or priority system
+int water_count = 0;
+
+//tmr
+int count = 0;
+
+//relay normally open,so when closed not enough moisture
+int relay;
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  moistureMoneyPlant();
+  moistureAlocasia();
+  moisturePathos();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+int moistureMoneyPlant(){
+  //read of soil
+  int moistureLevelCMP = analogRead(A0);
+
+  //need to find baseline for the moisture level for each plant, 30 arbitrary number
+
+  if (moistureLevelCMP <= 300){
+    water_count++;
+    if (water_count == 5){ //this number will be adjusted for plant
+      wateringFunction();
+      water_count = 0; //reassign
+    }
+  }
+};
+
+int moistureAlocasia(){
+  int moistureLevelA = analogRead(A1);
+  if (moistureLevelA <= 300){
+    water_count++;
+    if (water_count == 5){ //this number will be adjusted for plant
+     wateringFunction();
+     water_count = 0; //reassign
+   }
+  }
+};
+
+int moisturePathos(){
+  int moistureLevelP = analogRead(A2);
+  if (moistureLevelP <= 300){
+    water_count++;
+    if (water_count == 5){ //this number will be adjusted for plant
+     wateringFunction();
+      water_count = 0; //reassign
+    }
+  }
+};
+
+
+int wateringFunction() {
+  
+};
